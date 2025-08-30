f5946a57f6630f48918f3535de9d39a07a19fb20
mahmoud <mahmoudabdelhaleem90@gmail.com>
2025-08-30 14:32:10 +0300

    adding sign in form with validation

---
+++ src/Validations/signInSchema.ts
@@ -0,0 +1,15 @@
+import {z} from 'zod';
+
+const signInSchema = z.object({
+
+    email: z.string().min(1, { message: "Email address is required" }).email(),
+    password:z
+    .string()
+    .min(8,{ message: "Password must be at least 8 characters longs" })
+
+    })
+
+  
+type signInType = z.infer<typeof signInSchema>;
+
+export { signInSchema, type signInType };
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,60 @@
+import { Heading } from '@components/common';
+import { Col, Row } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { SubmitHandler,useForm } from 'react-hook-form';
+import { signInSchema,signInType } from 'src/Validations/signInSchema';
+import { zodResolver } from "@hookform/resolvers/zod";
+import Input from '@components/forms/Input/Input';
+
+
+
 
 export default function Login() {
+      const {
+        register,
+        handleSubmit,
+        formState: { errors },
+
+      } = useForm<signInType>({
+        mode:"onBlur",
+        resolver:zodResolver(signInSchema)
+      });
+      const submitForm:SubmitHandler<signInType> =(data) =>{
+        console.log(data);
+        
+      }
   return (
-    <div>
-      <p>login</p>
-    </div>
+    <>
+    <Heading title ="User Registration" />
+     <Row>
+      <Col md={{span:"6", offset:"3"}}>
+         <Form onSubmit={handleSubmit(submitForm)}>
+
+
+
+              <Input 
+              label='Email Address'
+              name ="email"
+              register={register}
+              error ={errors.email?.message}
+              />
+              <Input 
+              label='Password'
+              name ="password"
+              register={register}
+              error ={errors.password?.message}
+              />
+
+              <Button variant="info" type="submit" className='text-light'>
+                Submit
+              </Button>
+          </Form>
+      </Col>
+       
+      </Row>
+    </>
+     
+
   )
 }
